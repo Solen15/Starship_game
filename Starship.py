ba989c25517598bba141d9b25ea5a3a2944f9c38
Alex <sviridovsanya@gmail.com>
2023-05-06 22:26:35 -0700

    Pygame initial setup

---
+++ Starship.py
@@ -0,0 +1,29 @@
+import sys
+
+import pygame
+
+
+class Starship:
+    """Main class of the game"""
+
+    def __init__(self):
+        """Game initialization"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Starship")
+
+    def run_game(self):
+        """Launch main loop for the game"""
+        while True:
+            # wait for events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # update screen
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    Starship().run_game()
